4a39dac08e9928ca80f1d637275f51162e023d30
John Griffin <johnog@gmail.com>
2015-01-08 21:50:16 -0500

    ns session controller & disable devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'sessions' }
+  devise_for :users, skip: :all
+  devise_scope :user do
+    namespace :api do
+      namespace :v1 do
+        resources :sessions, only: :create
+      end
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
