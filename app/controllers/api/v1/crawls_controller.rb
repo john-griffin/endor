a736a748e214ec21267ab9a8156ad789b4864ce3
John Griffin <johnog@gmail.com>
2015-01-16 15:38:16 -0500

    test crawls api, add 404 support

--- app/controllers/api/v1/crawls_controller.rb
+++ app/controllers/api/v1/crawls_controller.rb
@@ -2,5 +2,7 @@ class Api::V1::CrawlsController < ApplicationController
   def show
     @crawl = Crawl.find(params[:id])
     render json: @crawl, serializer: CrawlV1Serializer, root: :crawl
+  rescue ActiveRecord::RecordNotFound
+    head 404, "content_type" => 'text/plain'
   end
 end
---
+++ spec/api/crawls_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Crawls API", :type => :request do
+  it "returns a crawl" do
+    crawl = Crawl.create!(name: 'my crawl')
+    get "/api/v1/crawls/#{crawl.id}"
+    response_data = JSON.parse(response.body)
+    expect(response_data).to eq({
+      'crawl' => {
+        'id' => crawl.id,
+        'name' => 'my crawl',
+        'links' => {
+          'stops' => "/api/v1/stops?crawl_id=#{crawl.id}"
+        }
+      }
+    })
+  end
+
+  it "crawl does not exist" do
+    get "/api/v1/crawls/1"
+    expect(response).to have_http_status(404)
+  end
+end
