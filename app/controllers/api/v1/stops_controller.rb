190aad7626e0fd2819e0af45dcc88326c637f699
John Griffin <johnog@gmail.com>
2015-01-16 13:48:38 -0500

    stop/venue creation api

--- app/controllers/api/v1/stops_controller.rb
+++ app/controllers/api/v1/stops_controller.rb
@@ -8,4 +8,34 @@ class Api::V1::StopsController < ApplicationController
 
     render json: @stops, each_serializer: StopV1Serializer
   end
+
+  def create
+    venue = Venue.new(venue_params)
+    @stop = Stop.new(stop_params.merge(venue: venue))
+
+    if @stop.save
+      render json: @stop, status: :created, root: :stop,
+             serializer: StopV1Serializer, location: [:api, :v1, @stop]
+    else
+      render json: { errors: @stop.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def stop_params
+    reqest_params.slice(:crawl_id, :name)
+  end
+
+  def venue_params
+    vp = reqest_params.slice(
+      :description, :venue_name, :photo_url, :foursquare_id, :location)
+    vp[:name] = vp.delete(:venue_name)
+    vp
+  end
+
+  def reqest_params
+    params.require(:stop).permit(:crawl_id, :name,
+      :description, :venue_name, :photo_url, :foursquare_id, location: [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       namespace :v1 do
         resources :sessions, only: :create
         resources :crawls, only: :show
-        resources :stops, only: :index
+        resources :stops, only: [:index, :create, :show]
       end
     end
   end
