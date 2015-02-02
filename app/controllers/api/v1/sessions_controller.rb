2fa23447da8822fc669aac5a89a205395a5e8b37
John Griffin <johnog@gmail.com>
2015-02-02 18:58:50 -0500

    redundant self

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ module Api
     class SessionsController < Devise::SessionsController
       def create
         self.resource = warden.authenticate(auth_options)
-        if self.resource
+        if resource
           sign_in(resource_name, resource)
           render json: resource, status: 201,
                  serializer: ResourceV1Serializer, root: false
