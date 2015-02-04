6fae62ed511e23a9e92caa2e26ddd7799ce1592f
John Griffin <johnog@gmail.com>
2015-02-03 23:01:23 -0500

    expect email field to just be called email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_filter :authenticate_user_from_token!
+  # before_filter :authenticate_user!
 
   private
 
   def authenticate_user_from_token!
     authenticate_with_http_token do |token, options|
-      user_email = options[:user_email].presence
+      user_email = options[:email].presence
       user       = user_email && User.find_by_email(user_email)
 
       if user && Devise.secure_compare(user.authentication_token, token)
