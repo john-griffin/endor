2e6600ecf93a4fb781ec96956429363e41c681b6
John Griffin <johnog@gmail.com>
2015-02-12 07:48:03 -0500

    add some postgres extensions

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207170329) do
+ActiveRecord::Schema.define(version: 20150212124700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "cube"
+  enable_extension "earthdistance"
 
   create_table "crawls", force: :cascade do |t|
     t.string   "name"
