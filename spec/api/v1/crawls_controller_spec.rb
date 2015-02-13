195cc63b8ef9a106732ef69287dcd93d7e86dd60
John Griffin <johnog@gmail.com>
2015-02-13 08:44:10 -0500

    refactor crawl spec

--- spec/api/v1/crawls_controller_spec.rb
+++ spec/api/v1/crawls_controller_spec.rb
@@ -3,37 +3,88 @@ require 'rails_helper'
 RSpec.describe Api::V1::CrawlsController do
   let(:user)  { User.create!(email: 'u@u.com', password: 'something') }
   let(:user2) { User.create!(email: 'a@a.com', password: 'aaaaaaaaa') }
-  let(:auth_header) do
-    generate_token(user)
-  end
+  let(:auth_header) { generate_token(user) }
 
-  it 'returns a crawl' do
-    crawl = Crawl.create!(name: 'my crawl', user: user, city: 'London')
-    get "/api/v1/crawls/#{crawl.id}"
-    expect(response).to have_http_status(200)
-    response_data = JSON.parse(response.body)
-    expect(response_data).to eq(
-      'crawl' => {
-        'id' => crawl.id,
-        'name' => 'my crawl',
-        'city' => 'London',
-        'user_id' => user.id,
-        'featured' => false,
-        'image_url' => nil,
-        'stop_count' => 0,
-        'links' => {
-          'stops' => "/api/v1/stops?crawl_id=#{crawl.id}"
+  context 'given an existing crawl' do
+    let(:crawl) { Crawl.create!(name: 'my crawl', user: user, city: 'London') }
+
+    it 'show action returns a crawl' do
+      get "/api/v1/crawls/#{crawl.id}"
+      expect(response).to have_http_status(200)
+      response_data = JSON.parse(response.body)
+      expect(response_data).to eq(
+        'crawl' => {
+          'id' => crawl.id,
+          'name' => 'my crawl',
+          'city' => 'London',
+          'user_id' => user.id,
+          'featured' => false,
+          'image_url' => nil,
+          'stop_count' => 0,
+          'links' => {
+            'stops' => "/api/v1/stops?crawl_id=#{crawl.id}"
+          }
         }
-      }
-    )
+      )
+    end
+
+    it 'update action can update a crawl' do
+      put "/api/v1/crawls/#{crawl.id}", {
+        crawl: {
+          name: 'Crawl 2', city: 'City 2'
+        }
+      }, auth_header
+      expect(response).to have_http_status(200)
+      response_data = JSON.parse(response.body)
+      expect(response_data).to eq({})
+      crawl.reload
+      expect(crawl.name).to eq('Crawl 2')
+      expect(crawl.city).to eq('City 2')
+    end
+
+    it 'update action is rejected with bad data' do
+      put "/api/v1/crawls/#{crawl.id}", {
+        crawl: {
+          name: nil
+        }
+      }, auth_header
+      expect(response).to have_http_status(422)
+    end
+
+    it 'update action rejects updates from invlaid user' do
+      put "/api/v1/crawls/#{crawl.id}", {
+        crawl: {
+          name: 'Crawl 2', city: 'City 2'
+        }
+      }, generate_token(user2)
+      expect(response).to have_http_status(401)
+    end
+
+    it 'destroy action can delete an owned crawl with associated stops' do
+      v1 = Venue.create!(foursquare_id: 'blah', point: [1, 1])
+      crawl.stops.create!(
+        venue: v1, photo_prefix: 'a', photo_suffix: 'b', photo_id: 'c')
+      delete "/api/v1/crawls/#{crawl.id}", {}, auth_header
+      expect(response).to have_http_status(204)
+    end
+
+    it 'destroy action cannot delete someone elses crawl' do
+      delete "/api/v1/crawls/#{crawl.id}", {}, generate_token(user2)
+      expect(response).to have_http_status(401)
+    end
+
+    it 'destroy action cannot delete a crawl without authenticated' do
+      delete "/api/v1/crawls/#{crawl.id}"
+      expect(response).to have_http_status(401)
+    end
   end
 
-  it 'crawl does not exist' do
+  it 'show action fails when crawl does not exist' do
     get '/api/v1/crawls/1'
     expect(response).to have_http_status(404)
   end
 
-  it 'can create a crawl' do
+  it 'create action can create a crawl' do
     post '/api/v1/crawls', {
       crawl: {
         name: 'Foo', city: 'bar', image_url: 'foo.jpg'
@@ -57,49 +108,13 @@ RSpec.describe Api::V1::CrawlsController do
       })
   end
 
-  it 'can not create a crawl with missing fields' do
+  it 'create action is rejected when fields are missing' do
     post '/api/v1/crawls', {
       crawl: {
         name: 'Foo'
       }
     }, auth_header
     expect(response).to have_http_status(422)
-  end
-
-  it 'can update a crawl' do
-    crawl = Crawl.create!(name: 'Crawl 1', city: 'City 1', user: user)
-    put "/api/v1/crawls/#{crawl.id}", {
-      crawl: {
-        name: 'Crawl 2', city: 'City 2'
-      }
-    }, auth_header
-    expect(response).to have_http_status(200)
-    response_data = JSON.parse(response.body)
-    expect(response_data).to eq({})
-    crawl.reload
-    expect(crawl.name).to eq('Crawl 2')
-    expect(crawl.city).to eq('City 2')
-  end
-
-  it 'can not update a crawl with bad data' do
-    crawl = Crawl.create!(name: 'Crawl 1', city: 'City 1', user: user)
-    put "/api/v1/crawls/#{crawl.id}", {
-      crawl: {
-        name: nil
-      }
-    }, auth_header
-    expect(response).to have_http_status(422)
-  end
-
-  it 'rejects updates from wrong user' do
-    user2 = User.create!(email: 'a@a.com', password: 'aaaaaaaa')
-    crawl = Crawl.create!(name: 'Crawl 1', city: 'City 1', user: user2)
-    put "/api/v1/crawls/#{crawl.id}", {
-      crawl: {
-        name: 'Crawl 2', city: 'City 2'
-      }
-    }, auth_header
-    expect(response).to have_http_status(401)
   end
 
   context 'given some featured and personal crawls' do
@@ -117,7 +132,7 @@ RSpec.describe Api::V1::CrawlsController do
         name: 'Crawl 4', city: 'New York', user: user2, featured: true)
     end
 
-    it 'returns only featured crawls when not logged in' do
+    it 'index action returns only featured crawls when not logged in' do
       get '/api/v1/crawls'
       expect(response).to have_http_status(200)
       response_data = JSON.parse(response.body)
@@ -136,7 +151,7 @@ RSpec.describe Api::V1::CrawlsController do
       )
     end
 
-    it 'returns featured and owned crawls when logged in' do
+    it 'index action returns featured and owned crawls when logged in' do
       get '/api/v1/crawls', {}, auth_header
       expect(response).to have_http_status(200)
       response_data = JSON.parse(response.body)
@@ -153,24 +168,6 @@ RSpec.describe Api::V1::CrawlsController do
           'stops' => "/api/v1/stops?crawl_id=#{crawl3.id}"
         }
       )
-    end
-
-    it 'can delete an owned crawl with stops' do
-      v1 = Venue.create!(foursquare_id: 'blah', point: [1,1])
-      crawl1.stops.create!(
-        venue: v1, photo_prefix: 'a', photo_suffix: 'b', photo_id: 'c')
-      delete "/api/v1/crawls/#{crawl1.id}", {}, auth_header
-      expect(response).to have_http_status(204)
-    end
-
-    it 'can not delete someone elses crawl' do
-      delete "/api/v1/crawls/#{crawl2.id}", {}, auth_header
-      expect(response).to have_http_status(401)
-    end
-
-    it 'can not delete a crawl unauthenticated' do
-      delete "/api/v1/crawls/#{crawl1.id}"
-      expect(response).to have_http_status(401)
     end
   end
 end
